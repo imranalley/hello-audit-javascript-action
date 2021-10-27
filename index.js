55187c7371931c459dc3afb5d2b2d57304bfc629
Imran Ali <imranrali1@gmail.com>
2021-10-27 15:40:28 -0400

    adding conversion

--- index.js
+++ index.js
@@ -1,12 +1,52 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 
-function convertToCSV(arr) {
-  const array = [Object.keys(arr[0])].concat(arr)
+function convertToCSV(objArray) {
+  var array = typeof objArray != 'object' ? JSON.parse(objArray) : objArray;
+  var str = '';
 
-  return array.map(it => {
-    return Object.values(it).toString()
-  }).join('\n')
+  for (var i = 0; i < array.length; i++) {
+      var line = '';
+      for (var index in array[i]) {
+          if (line != '') line += ','
+
+          line += array[i][index];
+      }
+
+      str += line + '\r\n';
+  }
+
+  return str;
+}
+
+function exportCSVFile(headers, items, fileTitle) {
+  if (headers) {
+      items.unshift(headers);
+  }
+
+  // Convert Object to JSON
+  var jsonObject = JSON.stringify(items);
+
+  var csv = this.convertToCSV(jsonObject);
+
+  var exportedFilenmae = fileTitle + '.csv' || 'export.csv';
+
+  var blob = new Blob([csv], { type: 'text/csv;charset=utf-8;' });
+  if (navigator.msSaveBlob) { // IE 10+
+      navigator.msSaveBlob(blob, exportedFilenmae);
+  } else {
+      var link = document.createElement("a");
+      if (link.download !== undefined) { // feature detection
+          // Browsers that support HTML5 download attribute
+          var url = URL.createObjectURL(blob);
+          link.setAttribute("href", url);
+          link.setAttribute("download", exportedFilenmae);
+          link.style.visibility = 'hidden';
+          document.body.appendChild(link);
+          link.click();
+          document.body.removeChild(link);
+      }
+  }
 }
 
 try {
@@ -19,9 +59,8 @@ try {
   // TODO add a parser script that converts to csv
   const payload = JSON.stringify(github.context.payload, undefined, 2)
   console.log(`The event payload: ${payload}`);
-  console.log(
-    convertToCSV(payload)
-  )
+  exportCSVFile(payload, test)
+
 } catch (error) {
   core.setFailed(error.message);
 }
