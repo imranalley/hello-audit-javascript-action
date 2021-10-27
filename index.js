83fc8b5291a7a51350a9fb31bad33f141c5ba59f
Imran Ali <imranrali1@gmail.com>
2021-10-27 15:17:22 -0400

    adding conversion to csv

--- index.js
+++ index.js
@@ -8,8 +8,10 @@ try {
   const time = (new Date()).toTimeString();
   core.setOutput("time", time);
   // Get the JSON webhook payload for the event that triggered the workflow
+  // TODO add a parser script that converts to csv
   const payload = JSON.stringify(github.context.payload, undefined, 2)
   console.log(`The event payload: ${payload}`);
+  $('#csv').text(ConvertToCSV(payload));
 } catch (error) {
   core.setFailed(error.message);
 }
