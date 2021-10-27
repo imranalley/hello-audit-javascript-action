63a1bafad478739082cc26ecd692610fc8fc5a0e
Imran Ali <imranrali1@gmail.com>
2021-10-27 15:57:42 -0400

    v1

--- index.js
+++ index.js
@@ -1,54 +1,6 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 
-function convertToCSV(objArray) {
-  var array = typeof objArray != 'object' ? JSON.parse(objArray) : objArray;
-  var str = '';
-
-  for (var i = 0; i < array.length; i++) {
-      var line = '';
-      for (var index in array[i]) {
-          if (line != '') line += ','
-
-          line += array[i][index];
-      }
-
-      str += line + '\r\n';
-  }
-
-  return str;
-}
-
-function exportCSVFile(items, fileTitle) {
-  // if (headers) {
-  //     items.unshift(headers);
-  // }
-
-  // Convert Object to JSON
-  var jsonObject = JSON.stringify(items);
-
-  var csv = this.convertToCSV(jsonObject);
-
-  var exportedFilenmae = fileTitle + '.csv' || 'export.csv';
-
-  var blob = new Blob([csv], { type: 'text/csv;charset=utf-8;' });
-  if (navigator.msSaveBlob) { // IE 10+
-      navigator.msSaveBlob(blob, exportedFilenmae);
-  } else {
-      var link = document.createElement("a");
-      if (link.download !== undefined) { // feature detection
-          // Browsers that support HTML5 download attribute
-          var url = URL.createObjectURL(blob);
-          link.setAttribute("href", url);
-          link.setAttribute("download", exportedFilenmae);
-          link.style.visibility = 'hidden';
-          document.body.appendChild(link);
-          link.click();
-          document.body.removeChild(link);
-      }
-  }
-}
-
 try {
   // `who-to-greet` input defined in action metadata file
   const nameToGreet = core.getInput('who-to-greet');
@@ -59,9 +11,6 @@ try {
   // TODO add a parser script that converts to csv
   const payload = JSON.stringify(github.context.payload, undefined, 2)
   console.log(`The event payload: ${payload}`);
-  var fileTile = 'test';
-  exportCSVFile(payload, fileTile)
-
 } catch (error) {
   core.setFailed(error.message);
 }
