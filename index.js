968f7a8bf45b687fec002c002aa7375db820f4d0
Imran Ali <imranrali1@gmail.com>
2021-10-27 15:24:19 -0400

    test

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 
+function convertToCSV(arr) {
+  const array = [Object.keys(arr[0])].concat(arr)
+
+  return array.map(it => {
+    return Object.values(it).toString()
+  }).join('\n')
+}
+
 try {
   // `who-to-greet` input defined in action metadata file
   const nameToGreet = core.getInput('who-to-greet');
@@ -11,7 +19,9 @@ try {
   // TODO add a parser script that converts to csv
   const payload = JSON.stringify(github.context.payload, undefined, 2)
   console.log(`The event payload: ${payload}`);
-  $('#csv').text(ConvertToCSV(payload));
+  console.log(
+    convertToCSV(payload)
+  )
 } catch (error) {
   core.setFailed(error.message);
 }
