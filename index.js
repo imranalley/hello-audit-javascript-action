1b6eeb6c4b6333ebfa726230f975c8c24969c1ae
Imran Ali <imranrali1@gmail.com>
2021-11-01 15:48:40 -0400

    adding csv file convert

--- index.js
+++ index.js
@@ -1,55 +1,41 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 const artifact = require('@actions/artifact');
-const { JSONtoCSV } = require("./utils")
-const fs = require("fs");
-const writeFileAsync = promisify(fs.writeFile);
-
-function createandUploadArtifacts() {
-  if (!process.env.GITHUB_RUN_NUMBER) {
-    return core.debug("not running in actions, skipping artifact upload");
-  }
-
-  const artifactClient = artifact.create();
-  const artifactName = `user-report-${new Date().getTime()}`;
-  const files = [
-    `./data/${ARTIFACT_FILE_NAME}.json`,
-    `./data/${ARTIFACT_FILE_NAME}.csv`
-  ];
-  const rootDirectory = "./";
-  const options = { continueOnError: true };
-
-  const uploadResult = await artifactClient.uploadArtifact(
-    artifactName,
-    files,
-    rootDirectory,
-    options
-  );
-  return uploadResult;
-}
+const axios = require('axios');
+const { parse } = require('json2csv');
+const fs = require('fs')
 
 try {
   // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput('who-to-greet');
-  console.log(`Hello ${nameToGreet}!`);
+  const creds = core.getInput('creds');
+  // run api call
+
+  const config = {
+    method: 'get',
+    url: 'https://api.github.com/user',
+    headers: {
+      'Authorization': `token ${creds}`
+    }
+  };
+  axios(config)
+
+  .then(function (response) {
+    console.log(response);
+    const res = response.data;
+    const fields = ['name', 'id', 'company', 'public_repos'];
+    const opts = { fields };
+    const csv = parse(res, opts);
+    console.log(csv);
+    fs.writeFile('response_nodejs.csv', csv, { flag: 'a+' }, err => {})
+    console.log(csv);
+  })
+
   const time = (new Date()).toTimeString();
   core.setOutput("time", time);
   // Get the JSON webhook payload for the event that triggered the workflow
   // TODO add a parser script that converts to csv
   const payload = JSON.stringify(github.context.payload, undefined, 2)
   console.log(`The event payload: ${payload}`);
- 
-  const csv = JSONtoCSV(payload)
-  await writeFileAsync(
-    `${DATA_FOLDER}/${ARTIFACT_FILE_NAME}.json`,
-    JSON.stringify(json)
-  );
-
-  console.log('testing writefile');
-  await writeFileAsync(`${DATA_FOLDER}/${ARTIFACT_FILE_NAME}.csv`, csv);
-
-  console.log('create and upload artifacts');
-  await createandUploadArtifacts();
   
 } catch (error) {
   core.setFailed(error.message);
